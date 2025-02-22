3a1248aa73e294ec5841d5b6425c71583694e7fe
TegarDiscovery <tegarpenemuan0@gmail.com>
2025-02-22 10:38:32 +0700

    chore(mailpit): setup MailPit di laravel dan testing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-email', function () {
+    Mail::raw('Test email dari Laravel + MailPit', function ($msg) {
+        $msg->to('test@example.com')->subject('Test Subject');
+    });
+
+    return 'Email sent!';
 });
